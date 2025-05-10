264853d0497a077a7a1fc1584f1149675916dc90
RomanMaklakov333 <romanmaklakoa@gmail.com>
2025-05-10 19:07:46 +0400

    Создал прототип игры "Пинг-понг"

--- ping_pong.py
+++ ping_pong.py
@@ -12,9 +12,48 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
+    def update_r(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+run = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+racket1 = Player('racket.png', 10, 200, 50, 150, 4)
+racket2 = Player('racket.png', 540, 200, 50, 150, 4)
+
+speed_x = 3
+speed_y = 3
+
+while run:
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+
+        racket1.reset()
+        racket2.reset()
+
+    display.update()
+    clock.tick(FPS)
